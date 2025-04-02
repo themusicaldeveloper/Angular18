e7e71a52ebdb9fee0a03b43ed7f2162686243569
ajay-solanki <ajay.solanki86@outlook.com>
2025-04-02 19:46:28 +0530

    1. Understand components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AddEmployeeComponent } from './components/add-employee/add-employee.component';
+import { EmployeeListComponent } from './components/employee-list/employee-list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AddEmployeeComponent, EmployeeListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/employee-list/employee-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-employee-list',
+  imports: [],
+  template: '<h3 class="primary">Employee List</h3>',
+  styles: ['.primary{color:red}']
+})
+export class EmployeeListComponent {
+
+}
