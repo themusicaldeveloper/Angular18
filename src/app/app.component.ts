b3e7649dfc3d5c2cb18f01e787b1208163fe3b20
themusicaldeveloper <ajay.solanki86@outlook.com>
2025-05-31 10:26:25 +0530

    2.2 Working on Attribute Directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { DataBindingComponent } from './components/data-binding/data-binding.component';
 import { StructuralDirComponent } from "./components/directive/structural-dir/structural-dir.component";
+import { AttributeDirComponent } from './components/directive/attribute-dir/attribute-dir.component';
 
 @Component({
   selector: 'app-root',
-  imports: [DataBindingComponent, StructuralDirComponent],
+  imports: [StructuralDirComponent, AttributeDirComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/directive/attribute-dir/attribute-dir.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-attribute-dir',
+  imports: [CommonModule,FormsModule],
+  templateUrl: './attribute-dir.component.html',
+  styleUrl: './attribute-dir.component.css'
+})
+export class AttributeDirComponent {
+
+}
