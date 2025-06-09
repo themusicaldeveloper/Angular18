f826bebc34cf2e8204a1bd6a941fee5fdf1c1f33
ajay-solanki <ajay.solanki86@outlook.com>
2025-06-09 20:16:21 +0530

    Attribute Directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { DataBindingComponent } from './components/data-binding/data-binding.component';
 import { StructuralDirComponent } from "./components/directive/structural-dir/structural-dir.component";
+import { AttributeDirComponent } from "./components/directive/attribute-dir/attribute-dir.component";
 
 @Component({
   selector: 'app-root',
-  imports: [DataBindingComponent, StructuralDirComponent],
+  imports: [DataBindingComponent, StructuralDirComponent, AttributeDirComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/directive/attribute-dir/attribute-dir.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+@Component({
+  selector: 'app-attribute-dir',
+  imports: [CommonModule,FormsModule],
+  templateUrl: './attribute-dir.component.html',
+  styleUrl: './attribute-dir.component.css'
+})
+export class AttributeDirComponent {
+  divBgColor:string = 'bg-primary';
+  isDiv2Active:boolean = false;
+
+  num1:string = '';
+  num2:string = ''; 
+
+
+
+studentList: any[] = [
+{ Id:1, TotalMarks:98, Gender:"male", Name:'Ajay', City:'Ahmedabad', Active:true},
+{ Id:2, TotalMarks:78, Gender:"male", Name:'Krishna', City:'Mathura', Active:false},
+{ Id:3, TotalMarks:65, Gender:"male", Name:'Raj', City:'Vadodara', Active:true},
+{ Id:4, TotalMarks:48, Gender:"female", Name:'Maheshwari', City:'Rishikesh', Active:false},
+{ Id:5, TotalMarks:28, Gender:"male", Name:'Shiv', City:'Kailash', Active:true},
+]
+
+
+
+  addRedClass(){
+    this.divBgColor="bg-danger";
+  }
+  addBlueClass(){
+    this.divBgColor="bg-primary";
+  }
+  toggleDiv2Active(){
+    this.isDiv2Active=!this.isDiv2Active;
+  }
+}
